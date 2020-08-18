1f4815ba1693a6a598385769e0aff8e5dee031b8
marekboro <marek.borowicz83@gmail.com>
2020-08-18 17:30:54 +0100

    added some functions to the park, now to create some dinos and a park in the park_spec

--- models/park.js
+++ models/park.js
@@ -1,5 +1,23 @@
+
+
 const JurassicPark = function (name,ticket_price,collection){
     this.name = name;
     this.ticket_price = ticket_price;
-    this.collection = collection;
+    this.collection = [];
+}
+
+JurassicPark.prototype.addDinosaur = function(dinosaur){
+    this.collection.push(dinosaur);
+}
+JurassicPark.prototype.removeDinosaur = function(dinosaur){
+    new_array = []
+    for (dino of this.collection){
+        if (!dinosaur){
+            new_array.push(dino);
+        }
+    } 
+    this.collection = new_array;
+}
+JurassicPark.prototype.removeDinosaur1 = function(dinosaur){
+    return;
 }
